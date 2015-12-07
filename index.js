e1752e0d2d948ad17b8fd2a9fffe9d67f19a4339
Diego Rodríguez <diegorbaquero@gmail.com>
2015-12-07 08:57:27 -0500

    Initial commit v1.0.0

---
+++ index.js
@@ -0,0 +1,62 @@
+module.exports = Storage
+
+var localStorage = require('localStorage')
+
+function Storage (chunkLength, opts) {
+  if (!(this instanceof Storage)) return new Storage(chunkLength, opts)
+  if (!opts) opts = {}
+
+  this.chunkLength = Number(chunkLength)
+  this.prefix = opts.files ? opts.files[0].path : Math.random().toString(36)
+  if (!this.chunkLength) throw new Error('First argument must be a chunk length')
+
+  this.closed = false
+  this.length = Number(opts.length) || Infinity
+
+  if (this.length !== Infinity) {
+    this.lastChunkLength = (this.length % this.chunkLength) || this.chunkLength
+    this.lastChunkIndex = Math.ceil(this.length / this.chunkLength) - 1
+  }
+}
+
+Storage.prototype.put = function (index, buf, cb) {
+  if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+  var isLastChunk = (index === this.lastChunkIndex)
+  if (isLastChunk && buf.length !== this.lastChunkLength) {
+    return nextTick(cb, new Error('Last chunk length must be ' + this.lastChunkLength))
+  }
+  if (!isLastChunk && buf.length !== this.chunkLength) {
+    return nextTick(cb, new Error('Chunk length must be ' + this.chunkLength))
+  }
+  localStorage.setItem(this.prefix + '_' + index, JSON.stringify(buf))
+  nextTick(cb, null)
+}
+
+Storage.prototype.get = function (index, opts, cb) {
+  if (typeof opts === 'function') return this.get(index, null, opts)
+  if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+  if (localStorage.getItem(this.prefix + '_' + index) == null) return nextTick(cb, new Error('Chunk not found'))
+  var buf = new Buffer(JSON.parse(localStorage.getItem(this.prefix + '_' + index)).data)
+  if (!buf) return nextTick(cb, new Error('Chunk not found'))
+  if (!opts) return nextTick(cb, null, buf)
+  var offset = opts.offset || 0
+  var len = opts.length || (buf.length - offset)
+  nextTick(cb, null, buf.slice(offset, len + offset))
+}
+
+Storage.prototype.close = Storage.prototype.destroy = function (cb) {
+  if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+  this.closed = true
+  for (var key in localStorage) {
+    if (key.startsWith(this.prefix)) {
+      localStorage.removeItem(key)
+    }
+  }
+  nextTick(cb, null)
+}
+
+function nextTick (cb, err, val) {
+  process.nextTick(function () {
+    if (cb) cb(err, val)
+  })
+}
